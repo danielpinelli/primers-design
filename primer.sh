88787bd28f5a5bfeca3a6574673ff2396c8d93a4
root <root@DESKTOP-DI0FSDD.localdomain>
2019-07-10 10:14:12 +0200

    rm primersh

--- primer.sh
+++
@@ -1,6 +0,0 @@
-## test obiconvert
-#test sur l'enregistrement
-
-
-## convert embl database into ecopcr 
-obiconvert --skip-on-error --embl -t /root/bureau/projet1/PrimerDesign-master/PrimerDesign-master/taxo/TAXO/ --ecopcrdb-output="testobiconvert" rel_est_env_01_r140.dat
