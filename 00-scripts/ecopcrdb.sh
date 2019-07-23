bf1c6a4047e1f67eedfcc92afb680f6ea2c3f413
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-07-23 18:21:46 +0200

    new primer to test

--- 00-scripts/ecopcrdb.sh
+++ 00-scripts/ecopcrdb.sh
@@ -16,17 +16,20 @@ ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL2_F $MS_DL2_R 
 ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL3_F $MS_DL3_R > mullus_std_MS_DL3.ecopcr
 ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 300 $MS_DL4_F $MS_DL4_R > mullus_std_MS_DL4.ecopcr
 
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 140 $MS_DL5_F $MS_DL5_R > mullus_std_MS_DL5.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 257 $MS_DL6_F $MS_DL6_R > mullus_std_MS_DL6.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 220 $MS_DL7_F $MS_DL7_R > mullus_std_MS_DL7.ecopcr
 
 
 ## in silico PCR testing 4 primers F/R
 ## 1
-ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL1_F $MS_DL1_R > pcr_MS_DL1.ecopcr
+#ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL1_F $MS_DL1_R > pcr_MS_DL1.ecopcr
 ## 2
-ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL2_F $MS_DL2_R > pcr_MS_DL2.ecopcr
+#ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL2_F $MS_DL2_R > pcr_MS_DL2.ecopcr
 ## 3
-ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL3_F $MS_DL3_R > pcr_MS_DL3.ecopcr
+#ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL3_F $MS_DL3_R > pcr_MS_DL3.ecopcr
 ## 4
-ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 300 $MS_DL4_F $MS_DL4_R > pcr_MS_DL4.ecopcr
+#ecoPCR -r $TELEOSTEI_TAXID -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 300 $MS_DL4_F $MS_DL4_R > pcr_MS_DL4.ecopcr
 
 
 ## convert custom FASTA.taxid into ecopcrdb
