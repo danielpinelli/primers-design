587bc96d5e7af5ef1923f6e4ebddcaea60174260
root <root@DESKTOP-DI0FSDD.localdomain>
2019-08-19 16:30:01 +0200

    script ecoprimer

--- 00-scripts/ecoprimers.sh
+++ 00-scripts/ecoprimers.sh
@@ -7,7 +7,4 @@
 ## load global variables
 source ./01-infos/config.sh
 
-ecoPrimers -d $ECOPCRDAT -e 3 -l 50 -L 120 -r 87757 -3 2 > mullus01.ecoprimers
-
-ecoPCR -d $ECOPCRDAT/mullos -e 3 -l 100 -L 130 CTGATATAGGACACGATA TAGCGGTCATCAGTTCTA > mysequences.ecopcr
-
+ecoPrimers -d $ECOPCRDAT -e 3 -l 50 -L 150 -r 87757 -3 2 > mullus01.ecoprimers
