83365c4381d4d4c4fe5af5fb8d826c9a4c519f39
root <root@DESKTOP-DI0FSDD.localdomain>
2019-08-19 16:27:19 +0200

    script ecopcr

--- 00-scripts/ecopcrdb.sh
+++ 00-scripts/ecopcrdb.sh
@@ -11,20 +11,19 @@ source ./01-infos/config.sh
 obiconvert --skip-on-error --embl -t $TAXD --ecopcrdb-output=$ECOPCRDAT $EMBLDAT mullus.embl.id.dat
 
 ## in silico PCR testing primers F/R
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL1_F $MS_DL1_R > mullus_std_MS_DL1.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL2_F $MS_DL2_R > mullus_std_MS_DL2.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL3_F $MS_DL3_R > mullus_std_MS_DL3.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 300 $MS_DL4_F $MS_DL4_R > mullus_std_MS_DL4.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL1_F $MS_DL1_R > mullus_std_MS_DL1.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL2_F $MS_DL2_R > mullus_std_MS_DL2.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL3_F $MS_DL3_R > mullus_std_MS_DL3.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL4_F $MS_DL4_R > mullus_std_MS_DL4.ecopcr
 
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 140 $MS_DL5_F $MS_DL5_R > mullus_std_MS_DL5.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 257 $MS_DL6_F $MS_DL6_R > mullus_std_MS_DL6.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L 220 $MS_DL7_F $MS_DL7_R > mullus_std_MS_DL7.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL5_F $MS_DL5_R > mullus_std_MS_DL5.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL6_F $MS_DL6_R > mullus_std_MS_DL6.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL7_F $MS_DL7_R > mullus_std_MS_DL7.ecopcr
 
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL9_F $MS_DL9_R > mullus_std_MS_DL9.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL10_F $MS_DL10_R > mullus_std_MS_DL10.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL11_F $MS_DL11_R > mullus_std_MS_DL11.ecopcr
-ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -l $ecoPCR_l -L $ecoPCR_L $MS_DL12_F $MS_DL12_R > mullus_std_MS_DL12.ecopcr
-
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL8_F $MS_DL8_R > mullus_std_MS_DL8.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL9_F $MS_DL9_R > mullus_std_MS_DL9.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL10_F $MS_DL10_R > mullus_std_MS_DL10.ecopcr
+ecoPCR -d $ECOPCRDAT -e $ecoPCR_e -L $ecoPCR_L $MS_DL11_F $MS_DL11_R > mullus_std_MS_DL11.ecopcr
 
 
 ## in silico PCR testing 4 primers F/R
